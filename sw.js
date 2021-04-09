e84cd491d3fae8e8495ac10bb517753e40463621
Eryk Sikora <e.sikora95@gmail.com>
2021-04-09 19:12:07 +0200

    some fixes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-navigator.serviceWorker.register("./sw.js").then(
+navigator.serviceWorker.register("./sw.js", { scope: "/" }).then(
   function (registration) {
     console.log("Service worker registration succeeded:", registration);
   },
