7982b45bd7bd47a9be7d46c09836fdc20791ef39
Eryk Sikora <e.sikora95@gmail.com>
2021-04-09 19:07:44 +0200

    some fixes

--- sw.js
+++ sw.js
@@ -1,14 +1,8 @@
-if ("serviceWorker" in navigator) {
-  // Register a service worker hosted at the root of the
-  // site using the default scope.
-  navigator.serviceWorker.register("./sw.js").then(
-    function (registration) {
-      console.log("Service worker registration succeeded:", registration);
-    },
-    /*catch*/ function (error) {
-      console.log("Service worker registration failed:", error);
-    }
-  );
-} else {
-  console.log("Service workers are not supported.");
-}
+navigator.serviceWorker.register("./sw.js").then(
+  function (registration) {
+    console.log("Service worker registration succeeded:", registration);
+  },
+  /*catch*/ function (error) {
+    console.log("Service worker registration failed:", error);
+  }
+);
