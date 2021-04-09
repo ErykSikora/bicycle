d01088e8dfba859de5450158cd6f700d88948e20
Eryk Sikora <e.sikora95@gmail.com>
2021-04-09 19:35:15 +0200

    some fixes

--- sw.js
+++ sw.js
@@ -1,8 +1,24 @@
-navigator.serviceWorker.register("./sw.js").then(
-  function (registration) {
-    console.log("Service worker registration succeeded:", registration);
-  },
-  /*catch*/ function (error) {
-    console.log("Service worker registration failed:", error);
-  }
-);
+var CACHE_NAME = "bicycle-cache";
+var urlsToCache = ["/", "/img"];
+
+self.addEventListener("install", function (event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
